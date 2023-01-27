19e511132a4ef67eed098b2b1bd457d30a5d6f2c
waleed bakar <waleedbakar@outlook.com>
2023-01-27 16:08:18 +0000

    update

--- variables.py
+++ variables.py
@@ -26,9 +26,13 @@ print(type(b))
 # print (a)
 
 # Getting user input
-print("hi, what is your name, age & DOB?")
-name_age_DOB = input()
-print("thank you")
+print("Hi, what is your name?")
+name = input()
+print ("Hi, " + name + " . How old are you?" )
+age = input()
+print("Can i also get your date of birth please?")
+DOB = input(str())
+print ("Hi, " + name + " you are " + age + " and was born on " + DOB)
 
 
 # Make a program that takes the users name, age and DOB and prints it back out.
