c140b8038e0c3aaafab64fa555a1f6731f369017
waleed bakar <waleedbakar@outlook.com>
2023-01-27 15:57:51 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,34 @@
+# Variables
+
+#variable_name = variable_data
+
+a = 1 #iint
+b = 2 #int
+c = 3.5 #float
+hi = "helllo world!" #float
+
+#Comment out muuliple lines with ctrl + /
+# print (hi)
+# print (a)
+
+
+# print (1 + 2)
+# print (a + b)
+
+# the type method
+
+print(type(b))
+
+# overwriting variables
+
+# print(a)
+# a = 4
+# print (a)
+
+# Getting user input
+print("hi, what is your name, age & DOB?")
+name_age_DOB = input()
+print("thank you")
+
+
+# Make a program that takes the users name, age and DOB and prints it back out.
